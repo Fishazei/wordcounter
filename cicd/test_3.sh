c706f8b0392b1fe154672568402f76d77b7dcf87
Fishazei <fishazei@mail.ru>
2026-02-23 02:40:25 +0300

    script update

--- cicd/test_3.sh
+++ cicd/test_3.sh
@@ -4,15 +4,15 @@
 echo "Тест 3: Специальные случаи"
 
 # Строка с дефисами и апострофами
-output1=$(echo "don't forget the 2nd test-case" | ./word_counter)
+output1=$(echo "don't forget the 2nd test-case" | ../usr/bin/wordcounter)
 expected1="Words in line: 5"
 
 # Пустая строка
-output2=$(echo "" | ./word_counter)
+output2=$(echo "" | ../usr/bin/wordcounter)
 expected2="Words in line: 0"
 
 # Только пробелы и знаки препинания
-output3=$(echo "   !!! ??? ,,,   " | ./word_counter)
+output3=$(echo "   !!! ??? ,,,   " | ../usr/bin/wordcounter)
 expected3="Words in line: 0"
 
 if [ "$output1" == "$expected1" ] && [ "$output2" == "$expected2" ] && [ "$output3" == "$expected3" ]; then
