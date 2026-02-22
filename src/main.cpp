099d60a60841ce060c8450a629938eede53f3838
Fishazei <fishazei@mail.ru>
2026-02-23 01:06:01 +0300

    rework main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,34 @@
 #include <cctype>
 #include <limits>
 using namespace std;
+
+/**
+ * Проверка, является ли символ разделителем слов
+ * Разделители: пробельные символы и знаки препинания
+ */
+bool isWordDelimiter(char c) {
+    // Пробельные символы
+    if (isspace(c)) return true;
+    // Знаки препинания, которые могут быть разделителями
+    const string punctuation = ".,!?;:-()\"'[]{}<>/\\|@#$%^&*+=~`";
+    return punctuation.find(c) != string::npos;
+}
+/**
+ * Проверка, является ли символ частью слова
+ * Буквы, цифры и дефис внутри слова
+ */
+bool isWordChar(char c, bool isFirstChar) {
+    // Буквы и цифры всегда часть слова
+    if (isalnum(c)) return true;
+
+    // Дефис может быть частью слова, если это не первый символ
+    if (c == '-' && !isFirstChar) return true;
+
+    // Апостроф может быть частью слова (например, "don't")
+    if (c == '\'' && !isFirstChar) return true;
+
+    return false;
+}
 /**
  * Функция для подсчета количества слов в строке
  * @param str входная строка
@@ -11,45 +39,92 @@ using namespace std;
 int countWords(const string& str) {
     int wordCount = 0;
     bool inWord = false;
-    for (char c : str) {
-        if (isspace(c)) {
-            inWord = false;
+    bool potentialWord = false;
+    int wordLength = 0;
+
+    for (size_t i = 0; i < str.length(); ++i) {
+        char c = str[i];
+        char nextChar = (i + 1 < str.length()) ? str[i + 1] : '\0';
+
+        if (isWordDelimiter(c)) {
+            // Если мы были в слове, заканчиваем его
+            if (inWord) {
+                // Проверяем, что слово состоит хотя бы из одного символа
+                if (wordLength > 0) {
+                    wordCount++;
+                }
+                inWord = false;
+                wordLength = 0;
+            }
+            potentialWord = false;
         }
         else {
-            if (!inWord) {
-                wordCount++;
-                inWord = true;
+            // Проверяем, может ли символ быть частью слова
+            if (isWordChar(c, wordLength == 0)) {
+                if (!inWord) {
+                    // Начало нового слова
+                    inWord = true;
+                    wordLength = 1;
+                }
+                else {
+                    // Продолжение слова
+                    wordLength++;
+                }
+            }
+            else {
+                // Символ не является частью слова
+                if (inWord) {
+                    // Завершаем текущее слово
+                    if (wordLength > 0) {
+                        wordCount++;
+                    }
+                    inWord = false;
+                    wordLength = 0;
+                }
             }
         }
     }
+
+    // Проверяем последнее слово
+    if (inWord && wordLength > 0) {
+        wordCount++;
+    }
+
     return wordCount;
 }
 /**
- * Проверка, что строка не превышает максимальную длину
- * и содержит только допустимые символы
+ * Проверка входных данных
  */
 bool validateInput(const string& str) {
     const size_t MAX_LENGTH = 1000;
     if (str.length() > MAX_LENGTH) {
-        cout << "Err! To much symbols" << endl;
-        return false;
-    }
-    if (str.empty()) {
-        cout << "Err! The string is empty" << endl;
+        cout << "Err! Too many symbols" << endl;
         return false;
     }
     return true;
 }
-int main() {
+/*  */
+int main(int argc, char* argv[]) {
     string input;
-    cout << "Count words in line" << endl;
-    cout << "Enter line: ";
-    // Безопасное чтение строки
-    getline(cin, input);
-    if (cin.fail()) {
-        cout << "Err!" << endl;
-        return 1;
+    // Проверяем аргументы командной строки
+    if (argc > 1) {
+        // Объединяем все аргументы в одну строку
+        for (int i = 1; i < argc; ++i) {
+            if (i > 1) input += " ";
+            input += argv[i];
+        }
     }
+    else {
+        // Интерактивный режим
+        cout << "type line: ";
+        getline(cin, input);
+
+        if (cin.fail()) {
+            cout << "Err! Input error" << endl;
+            return 1;
+        }
+    }
+    // Валидация строки
     if (!validateInput(input)) {
         return 1;
     }
