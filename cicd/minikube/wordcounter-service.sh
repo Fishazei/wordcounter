bffdfc84762286ed373ce8fbff479062128699dc
Fishazei <fishazei@mail.ru>
2026-03-06 02:50:12 +0300

    service remake

--- cicd/minikube/wordcounter-service.sh
+++ cicd/minikube/wordcounter-service.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-inputL=("Uno duo tres" "From Russia with love" "Aida Pushkin, aida sykin sin")
+inputL=("Uno duo tres" "From Russia with love" "Aida Pushkin, aida (menya zastavili ubrat 2nd chast citaty)")
 
 while true; do
 
 	for input in "${inputL[@]}"; do
 	echo "-----------------"
 	echo "input : $input"
-	#outp=$(wordcounter "$input")
-	#echo "output : $outp"
+	outp=$(wordcounter "$input")
+	echo "output : $outp"
 	echo "-----------------"
 	sleep 1.0
 	done
